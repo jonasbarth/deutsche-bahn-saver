651673061a82966fae27cb0125ff869a72c59876
jonasbarth <jb901@exeter.ac.uk>
2022-06-22 22:01:01 +0200

    Add package json and first files for trying to read from grpc api

---
+++ src/main.js
@@ -0,0 +1,48 @@
+var PROTO_PATH = 'C:/Users/Jonas/Desktop/Projects/deutsche-bahn/deutsche-bahn-data/deutsche-bahn-data-acquisition/src/main/proto/timetable.proto';
+var grpc = require('@grpc/grpc-js');
+var protoLoader = require('@grpc/proto-loader');
+const ObjectsToCsv = require('objects-to-csv');
+const cron = require('node-cron');
+
+var argv = require('minimist')(process.argv.slice(2));
+
+const eva = argv["eva"]
+
+
+// Suggested options for similarity to existing grpc.load behavior
+var packageDefinition = protoLoader.loadSync(
+    PROTO_PATH,
+    {keepCase: true,
+     longs: String,
+     enums: String,
+     defaults: true,
+     oneofs: true
+    });
+var protoDescriptor = grpc.loadPackageDefinition(packageDefinition);
+
+cron.schedule('*/2 * * * *', () => {
+    var client = new protoDescriptor.TimetableService('localhost:8080', grpc.credentials.createInsecure());
+
+    const timestamp = Math.round(Date.now() / 1000) - 120;
+
+    var request = {eva: eva, departedAfter: timestamp}
+
+    console.log(timestamp);
+    let responses = []
+
+    var call = client.getDepartedAfter(request);
+    call.on('data', function(response) {
+        console.log(response);
+        responses.push(response);
+    });
+    call.on('error', (err) => {
+        console.error(err);
+    });
+    call.on('end', () => {
+        if (responses.length != 0) {
+            const csv = new ObjectsToCsv(responses);
+            console.log(responses);
+            csv.toDisk(`./${eva}.csv`, { append: true });
+        }
+    })
+  });
