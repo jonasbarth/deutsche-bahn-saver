cc87e3108ed50945dd4fdce0398c2d34a34109e1
jonasbarth <jb901@exeter.ac.uk>
2022-07-19 19:46:16 +0200

    create file that loads all of the stations into the mongodb

--- src/database/init.js
+++ src/database/init.js
@@ -5,10 +5,15 @@ var MongoClient = require('mongodb').MongoClient;
 
 MongoClient.connect(url, function(err, db) {
     if (err) throw err;
-
     var dbo = db.db("deutsche-bahn");
     
-    dbo.createCollection("stops", function(err, res) {
+    dbo.createCollection("stop", function(err, res) {
+        if (err) throw err;
+        console.log("Collection created!");
+        db.close();
+    });
+
+    db.createCollection("station", function(err, res) {
         if (err) throw err;
         console.log("Collection created!");
         db.close();
